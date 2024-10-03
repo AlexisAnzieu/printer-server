32d36c71cd83209e00a48089e67b1f3e34b04480
alexis <alexis.anzieu@gmail.com>
2024-10-03 15:23:41 -0400

    make it sudo

--- wifi_check.sh
+++ wifi_check.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Check if connected to a Wi-Fi network
-if ! nmcli -t -f active,ssid dev wifi | egrep '^yes' | cut -d: -f2; then
+if ! sudo nmcli -t -f active,ssid dev wifi | egrep '^yes' | cut -d: -f2; then
     echo "Not connected to Wi-Fi. Starting hotspot..."
     # Stop any existing hotspot
-    nmcli con down Hotspot
+    sudo nmcli con down Hotspot
     # Create a new hotspot
-    nmcli dev wifi hotspot ifname wlan0 ssid PrinterHotspot password "printer"
+    sudo nmcli dev wifi hotspot ifname wlan0 ssid PrinterHotspot password "printer123"
 else
     echo "Connected to Wi-Fi."
 fi
